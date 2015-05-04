6deeac337a2021be2bc12c1a046161bb9a3f7e9a
John ShaggyTwoDope Jenkins <twodopeshaggy@gmail.com>
2015-05-04 01:34:11 -0700

    0.2.0 version update

--- update.sh
+++ update.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 export GOPATH=/home/linuxlite/drivedebpackage/
-go get github.com/odeke-em/drive/drive-gen
+go get github.com/odeke-em/drive/cmd/drive
+go get github.com/odeke-em/drive/config
+go get github.com/rakyll/command
