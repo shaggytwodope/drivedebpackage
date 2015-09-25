a58b771350a01c5ac082d7cb73ae17e91a55e423
John ShaggyTwoDope Jenkins <twodopeshaggy@gmail.com>
2015-09-25 03:38:24 -0700

    0.3.0 pre-release test

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export GOPATH=/home/linuxlite/drivedebpackage/
+export GOPATH=/home/shaggy//workspace/personal/drivedebpackage
 go get github.com/odeke-em/drive/cmd/drive
 go get github.com/odeke-em/drive/config
-go get github.com/rakyll/command
+go get github.com/odeke-em/command
