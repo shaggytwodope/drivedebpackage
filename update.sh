c99ba4793395672476403451b1f61089b0bf240d
John ShaggyTwoDope Jenkins <twodopeshaggy@gmail.com>
2015-10-22 21:14:38 -0700

    hacky changes

--- update.sh
+++ update.sh
@@ -3,3 +3,7 @@ export GOPATH=/home/shaggy/workspace/personal/drivedebpackagedir/drivedebpackage
 go get github.com/odeke-em/drive/cmd/drive
 go get github.com/odeke-em/drive/config
 go get github.com/odeke-em/command
+go get github.com/odeke-em/rsc/qr
+go get github.com/martini-contrib/binding
+go get github.com/odeke-em/drive/drive-server
+
